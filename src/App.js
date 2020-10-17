17eb522891301ab8f65a3ec12acb3da56b45ec38
Emerson <emersonteles21@gmail.com>
2020-10-17 20:59:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/Logo reta.svg';
 
 function App() {
+  const instagram = "https://www.instagram.com/pinusburguer/"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={logo} alt="logo pinus"/>
+        <h1>Em breve aqui o novo site Pinu's Burger!</h1>
+        <div className="Social">
+          <p>Nos acompanhe no instagram:</p>
+          <a href={instagram} target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-instagram" aria-hidden="true"> </i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
