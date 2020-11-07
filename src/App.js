f6910493a707ea2b68e84468c026f75682bdd737
Emerson <emersonteles21@gmail.com>
2020-11-07 16:29:14 -0300

    ajustando mapa

--- src/App.js
+++ src/App.js
@@ -27,7 +27,8 @@ function App() {
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d985.6069337860607!2d-48.016921170792656!3d-15.883054709407705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTXCsDUyJzU5LjAiUyA0OMKwMDAnNTkuMCJX!5e1!3m2!1spt-BR!2sbr!4v1604776385055!5m2!1spt-BR!2sbr" 
                   allowfullscreen="" 
                   aria-hidden="false" 
-                  tabindex="0">
+                  tabindex="0"
+                  title="Location">>
           </iframe>
       </div>
     </div>
