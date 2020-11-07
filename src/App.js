c47d847d7ecda01ab0c365741668f9d4efc19aec
Emerson <emersonteles21@gmail.com>
2020-11-07 15:50:45 -0300

    "Cardápio atualizado"

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ function App() {
   return (
     <div className="App">
       <div className="Content">
-        <img src={logo} alt="logo pinus"/>
-        <h1>Em breve aqui o novo site Pinu's Burger!</h1>
+        <div className="Head">
+          <img src={logo} alt="logo pinus"/>
+          <h1>Em breve aqui o novo site Pinu's Burger!</h1>
+        </div>
         <div className="Social">
           <p>Nos acompanhe no instagram:</p>
           <a href={instagram} target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-instagram" aria-hidden="true"> </i>
+          </a>
+        </div>
+        <div className="Cardapio">
+          <p>Confira nosso cardápio:</p>
+
+          <a href="https://ibb.co/hLjznq6" rel="noopener noreferrer" target="_blank" className="cardapio_link">
+            <i class="fa fa-book" aria-hidden="true"></i>
           </a>
         </div>
       </div>
