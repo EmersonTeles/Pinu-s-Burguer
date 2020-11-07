5da65dda68cd54da29ace07cdf9afd930f100ebc
Emerson <emersonteles21@gmail.com>
2020-11-07 16:48:48 -0300

    ajuste na tela

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/Logo reta.svg';
-
 function App() {
   const instagram = "https://www.instagram.com/pinusburguer/"
   return (
