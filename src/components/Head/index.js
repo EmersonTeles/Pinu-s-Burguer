ccedbb605bcfd0ee46da15e3a86426d2caece139
Emerson <emersonteles21@gmail.com>
2020-12-21 02:10:38 -0300

    First new site commit

---
+++ src/components/Head/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './styles.css';
+import logo from '../../assets/Logo site 2.svg';
+export default function Head(){
+    return(
+        <div className="Head">
+            <div className="HeadContent">
+                <img className="logo" src={logo} alt="Logo Head"/>
+                <ul className="Options">
+                    <li className="ItemLista">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="ItemLista">
+                        <a href="/">Cardápio</a>
+                    </li>
+                    <li className="ItemLista">
+                        <a href="/">Delivery</a>
+                    </li>
+                    <li className="ItemLista">
+                        <a href="/">Contato</a>
+                    </li>
+                    <li className="ItemLista">
+                        <a href="/">Sobre nós</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+} 
