9f7893f41c8c3d9c0e7f4f374985a5f535489b21
Emerson <emersonteles21@gmail.com>
2020-12-21 22:47:21 -0300

    updates footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Head from './components/Head/';
+import Footer from './components/Footer/';
 import fundo from './assets/foto de fundo.jpg';
 
 function App() {
   return (
     <div className="Content">
+
       <Head/>
-      <div>
+      <div className="divFundo">
         <img className="Fundo" src={fundo} alt= "fundo"/>
       </div>
+      <Footer/>
     </div>
   );
 }
