e26f30f65a826fb74904759e240709fccd8ec5eb
Emerson <emersonteles21@gmail.com>
2021-01-22 02:36:56 -0300

    correção de conflito

--- src/App.js
+++ src/App.js
@@ -8,34 +8,12 @@ import Contatos from './components/Contatos/';
 import Footer from './components/Footer/';
 function App() {
   return (
-<<<<<<< HEAD
     <div className="Content">
       <Head/>
       <Banner/>
       <Cardapio/>
       <Contatos/>
       <Footer/>
-=======
-    <div className="App">
-      <div className="Content">
-        <div className="Head">
-          <img src={logo} alt="logo pinus"/>
-          <h1>Em breve aqui o novo site Pinu's Burger!</h1>
-        </div>
-        <div className="Social">
-          <p>Nos acompanhe no instagram:</p>
-          <a href={instagram} target="_blank" rel="noopener noreferrer">
-                <i className="fa fa-instagram" aria-hidden="true"> </i>
-          </a>
-        </div>
-        <div className="Cardapio">
-          <p>Confira nosso cardápio:</p>
-          <a href="https://ibb.co/hLjznq6" rel="noopener noreferrer" target="_blank" className="cardapio_link">
-            <i class="fa fa-book" aria-hidden="true"></i>
-          </a>
-        </div>
-      </div>
->>>>>>> master
     </div>
   );
 }
